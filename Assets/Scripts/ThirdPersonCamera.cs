5eca400bb20fb1eea5bdf1a4dee73e268572dd6c
Christopher Herrera <christopher.h@futurelinkcorporation.com>
2015-04-10 19:00:57 -0400

    initial commit

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+	public GameObject target;
+	public float sensitivity = 5;
+	public float maximumZAxis = -35;
+	public float minimumZAxis = 20;
+
+	Vector3 offset;
+	float z,y;
+
+	void Start () {
+		offset = target.transform.position - transform.position;
+	}
+
+	void LateUpdate () {
+		float horizontal = Input.GetAxis("Mouse X") * sensitivity;
+		float vertical = Input.GetAxis("Mouse Y") * sensitivity;
+
+		y += horizontal;
+		z += vertical;
+
+		if (z <= maximumZAxis) {
+			z = maximumZAxis;
+		}
+		if (z >= minimumZAxis) {
+			z = minimumZAxis;
+		}
+	
+		Quaternion rotation = Quaternion.Euler(0, y, z);
+		transform.position = target.transform.position - (rotation * offset);
+		
+		transform.LookAt(target.transform);
+	}
+
+}
